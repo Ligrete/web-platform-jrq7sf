25e47bf5601924675fdf70731613f1733a9d5b55
Willy <pro4vayder@gmail.com>
2020-11-02 17:42:06 +0300

    path lines

--- path.js
+++ path.js
@@ -4,12 +4,6 @@ console.log('path.js')
 
 var scrH = 480;
 var scrW = 640;
-var offset = 50;
-
-var tsmitterX = 0;
-var tsmitterY = 0;
-
-var points = [];
 
 
 // // повторить с интервалом 2 секунды
@@ -17,73 +11,3 @@ var points = [];
 
 // // остановить вывод через 5 секунд
 // setTimeout(() => { clearInterval(timerId); console.log('stop'); }, 5000);
-
-
-genTargets();
-
-function genTargets() {
- for (let index = 0; index < 5; index++) {
-   points.push({
-    name: "point "+index,
-    x: Math.floor(Math.random() * (scrW-offset))+offset/2,
-    y: Math.floor(Math.random() * (scrH-offset))+offset/2
-  });
- }
-}
-
-
-function start() {
-  // test
-  var canvas = document.getElementById("canvasmove");
-  if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-    let i = 0;
-    for (point of points) {
-      if (i == 1)
-        ctx.beginPath();
-        ctx.fillStyle = "#ffc107";
-        var receiverX = point.x;
-        var receiverY = point.y;
-        ctx.arc(receiverX, receiverY, 6, 0, Math.PI*2 ,true);
-        ctx.closePath();
-        ctx.fill();
-        i++;
-    }
-  }
-}
-
-
-
-function drawpath() {
-    // test
-    var canvas = document.getElementById("canvaspath");
-    if (canvas.getContext) {
-      var ctx = canvas.getContext("2d");
-      clearPage(ctx, canvas);
-
-      for (point of points) {
-        console.log(point.name+" "+point.x+" "+point.y);
-        //target pos
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        
-        var receiverX = point.x;
-        var receiverY = point.y;
-        ctx.arc(receiverX, receiverY, 4, 0, Math.PI*2 ,true);
-        ctx.closePath();
-        ctx.fill();
-      }
-      start();
-    }
-}
-
-function clearPage(context, canvas) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    var w = canvas.width;
-    canvas.width = 1;
-    canvas.width = w;
-    context.closePath();
-    context.fill();
-
-}
-
--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-console.log('hello!')
-
 var scrH = 480;
 var scrW = 640;
 var offset = 50;
 
+var tsmitterX = 0;
+var tsmitterY = 0;
 
-let receivers = [
-    {
+var points = [{name: '', x: 111, y: 111}];
+
+
+var table; // таблица координат приемников
+
+
+let receivers = [{
         "name": "Yuka"
     },
     {
@@ -25,20 +30,79 @@ function draw() {
 
     var canvas = document.getElementById("canvas");
     if (canvas.getContext) {
-      var ctx = canvas.getContext("2d");
-      canvas.getContext("2d").clearRect(0, 0, scrW, scrH);
-      clearPage(ctx, canvas);
-      drawBoard(ctx, 20);
+        var ctx = canvas.getContext("2d");
+        canvas.getContext("2d").clearRect(0, 0, scrW, scrH);
+        clearPage(ctx, canvas);
+        //clear Table
+        table = document.getElementById("rtable");
+        if (table) {
+            table.innerHTML = "";
+        }
+        drawBoard(ctx, 20);
 
-      for (receiver of receivers) {
-        ctx.fillStyle = "#fd7e14";
-        var receiverX = Math.floor(Math.random() * (scrW-offset))+offset/2;
-        var receiverY = Math.floor(Math.random() * (scrH-offset))+offset/2;
-        ctx.arc(receiverX, receiverY, 5, 0, Math.PI*2 ,true);
-        ctx.closePath();
-        ctx.fill();
-        addCoords(receiver.name, receiverX, receiverY);
-      }
+        for (receiver of receivers) {
+            ctx.fillStyle = "#fd7e14";
+            var receiverX = Math.floor(Math.random() * (scrW - offset)) + offset / 2;
+            var receiverY = Math.floor(Math.random() * (scrH - offset)) + offset / 2;
+            ctx.arc(receiverX, receiverY, 5, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fill();
+            addCoords(receiver.name, receiverX, receiverY);
+        }
+    }
+}
+
+function drawpath() {
+    genTargets(3);
+    // test
+    var canvas = document.getElementById("canvaspath");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+        clearPage(ctx, canvas);
+
+
+        for (point of points) {
+            console.log('drawpath: ' + point.name + " " + point.x + " " + point.y);
+            //target pos
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            var receiverX = point.x;
+            var receiverY = point.y;
+            ctx.arc(receiverX, receiverY, 4, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fill();
+
+            //text
+            ctx.font = "12px serif";
+            ctx.fillStyle = "#dc3545";
+            ctx.fillText( point.name , point.x, point.y + 20);
+        }
+
+        var i = 0;
+        var prevX = -100;
+        var prevY = -100;
+
+
+        for (point of points) {
+            
+            console.log('test generator: '+prevX +' - '+prevY);
+            if (i>=0 && prevY>0 && prevX>0) {
+                ctx.beginPath();
+                ctx.strokeStyle = "#dc354582"; 
+                ctx.lineWidth = 5;
+                ctx.moveTo(prevX, prevY);
+                ctx.lineTo(point.x, point.y);
+                ctx.closePath();
+                ctx.stroke();
+                prevX = point.x;
+                prevY = point.y;
+            } else {
+                prevX = point.x;
+                prevY = point.y;
+            }
+            i++;
+        }
+        start();
     }
 }
 
@@ -47,12 +111,9 @@ function clearPage(context, canvas) {
     var w = canvas.width;
     canvas.width = 1;
     canvas.width = w;
-    //clear Table
-    var table = document.getElementById("rtable");
-    table.innerHTML = "";
 }
 
-function drawBoard(context, size){
+function drawBoard(context, size) {
     var p = 0;
     for (var x = 0; x <= scrW; x += size) {
         context.moveTo(0.5 + x + p, p);
@@ -79,3 +140,42 @@ function addCoords(rName, rX, rY) {
     cell2.innerHTML = rX;
     cell3.innerHTML = rY;
 }
+
+
+
+
+
+function genTargets(count) {
+    points.splice(0,points.length);
+    for (let index = 0; index < count; index++) {
+        points.push({
+            name: "point " + index,
+            x: Math.floor(Math.random() * (scrW - offset)) + offset / 2,
+            y: Math.floor(Math.random() * (scrH - offset)) + offset / 2
+        });
+    }
+}
+
+
+function start() {
+    // test
+    console.log('start');
+    var canvas = document.getElementById("canvaspath");
+    if (canvas.getContext) {    
+        var ctx = canvas.getContext("2d");
+        for (var i = 0; i <= points.length; i++) {
+            if (i == 0) {
+                console.log('start: trigger - ' + i);
+                ctx.beginPath();
+                ctx.fillStyle = "#ffc107";
+                var receiverX = points[i].x;
+                var receiverY = points[i].y;
+                ctx.arc(receiverX, receiverY, 6, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.fill();
+            }
+        }
+    }
+}
+
+genTargets(3);
